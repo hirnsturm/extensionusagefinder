6188fb3a084a53405aad65551b337627b95ee973
Steve Lenz <sle@xima.de>
2020-12-08 12:52:53 +0100

    [Task] Add new extension icon

--- ext_tables.php
+++ ext_tables.php
@@ -1,56 +1,61 @@
 <?php
-if (!defined('TYPO3_MODE')) {
-    die('Access denied.');
-}
 
-if (TYPO3_MODE === 'BE') {
+defined('TYPO3_MODE') || die('Access denied.');
 
-    /**
-     * Registers a Backend Module
-     */
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-        'Sle.'.$_EXTKEY, 'tools', // Make module a submodule of 'tools'
-        'extensionfinder', // Submodule key
-        '', // Position
-        array(
-        'Finder' => 'index',
-        ),
-        array(
-        'access'         => 'user,group',
-        'icon'           => 'EXT:'.$_EXTKEY.'/ext_icon.gif',
-        'labels'         => 'LLL:EXT:'.$_EXTKEY.'/Resources/Private/Language/locallang_extensionfinder.xlf',
-        //'navFrameScript' => 'alt_db_navframe.php' // Seitenbaum aktivieren
-        )
-    );
-}
+call_user_func(
+    function () {
+        if (TYPO3_MODE === 'BE') {
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY,
-    'Configuration/TypoScript', 'Extension Usage Finder');
+            /**
+             * Registers a Backend Module
+             */
+            \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+                'Sle.extensionusagefinder',
+                'tools',
+                'extensionfinder',
+                '',
+                [
+                    'Finder' => 'index',
+                ],
+                [
+                    'access' => 'user,group',
+                    'icon'   => 'EXT:extensionusagefinder/Resources/Public/Icons/Extension.svg',
+                    'labels' => 'LLL:EXT:extensionusagefinder/Resources/Private/Language/locallang_extensionfinder.xlf',
+                ]
+            );
+        }
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_extensionusagefinder_domain_model_finder',
-    'EXT:extensionusagefinder/Resources/Private/Language/locallang_csh_tx_extensionusagefinder_domain_model_finder.xlf');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_extensionusagefinder_domain_model_finder');
-$GLOBALS['TCA']['tx_extensionusagefinder_domain_model_finder'] = array(
-    'ctrl' => array(
-        'title'                    => 'LLL:EXT:extensionusagefinder/Resources/Private/Language/locallang_db.xlf:tx_extensionusagefinder_domain_model_finder',
-        'label'                    => 'uid',
-        'tstamp'                   => 'tstamp',
-        'crdate'                   => 'crdate',
-        'cruser_id'                => 'cruser_id',
-        'dividers2tabs'            => TRUE,
-        'versioningWS'             => 2,
-        'versioning_followPages'   => TRUE,
-        'languageField'            => 'sys_language_uid',
-        'transOrigPointerField'    => 'l10n_parent',
-        'transOrigDiffSourceField' => 'l10n_diffsource',
-        'delete'                   => 'deleted',
-        'enablecolumns'            => array(
-            'disabled'  => 'hidden',
-            'starttime' => 'starttime',
-            'endtime'   => 'endtime',
-        ),
-        'searchFields'             => '',
-        'dynamicConfigFile'        => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY).'Configuration/TCA/Finder.php',
-        'iconfile'                 => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY).'Resources/Public/Icons/tx_extensionusagefinder_domain_model_finder.gif'
-    ),
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+            'extensionusagefinder',
+            'Configuration/TypoScript', 'Extension Usage Finder'
+        );
+
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_extensionusagefinder_domain_model_finder',
+            'EXT:extensionusagefinder/Resources/Private/Language/locallang_csh_tx_extensionusagefinder_domain_model_finder.xlf');
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_extensionusagefinder_domain_model_finder');
+        $GLOBALS['TCA']['tx_extensionusagefinder_domain_model_finder'] = [
+            'ctrl' => [
+                'title'                    => 'LLL:EXT:extensionusagefinder/Resources/Private/Language/locallang_db.xlf:tx_extensionusagefinder_domain_model_finder',
+                'label'                    => 'uid',
+                'tstamp'                   => 'tstamp',
+                'crdate'                   => 'crdate',
+                'cruser_id'                => 'cruser_id',
+                'dividers2tabs'            => true,
+                'versioningWS'             => 2,
+                'versioning_followPages'   => true,
+                'languageField'            => 'sys_language_uid',
+                'transOrigPointerField'    => 'l10n_parent',
+                'transOrigDiffSourceField' => 'l10n_diffsource',
+                'delete'                   => 'deleted',
+                'enablecolumns'            => [
+                    'disabled'  => 'hidden',
+                    'starttime' => 'starttime',
+                    'endtime'   => 'endtime',
+                ],
+                'searchFields'             => '',
+                'dynamicConfigFile'        => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('extensionusagefinder') . 'Configuration/TCA/Finder.php',
+                'iconfile'                 => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('extensionusagefinder') . 'Resources/Public/Icons/tx_extensionusagefinder_domain_model_finder.gif',
+            ],
+        ];
+    }
 );
